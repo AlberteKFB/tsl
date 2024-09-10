d3b8907ddcd89367b4f3be19eea608a99942d83f
Adam <adje0001@stud.kea.dk>
2024-09-10 11:51:01 +0200

    Link til data

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 //https://qbkoftawrbmgqxsdzzqo.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia29mdGF3cmJtZ3F4c2R6enFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjcwMjUsImV4cCI6MjA0MTQ0MzAyNX0.WYYhWAAR - vH0tJEXbooEn5Wef00scdf - Qq8g8kiYFXs;
 
-fetch("https://qbkoftawrbmgqxsdzzqo.supabase.co/rest/v1/TSL", {
+//link med data
+//qbkoftawrbmgqxsdzzqo.supabase.co/rest/v1/TSL?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia29mdGF3cmJtZ3F4c2R6enFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjcwMjUsImV4cCI6MjA0MTQ0MzAyNX0.WYYhWAAR-vH0tJEXbooEn5Wef00scdf-Qq8g8kiYFXs
+https: fetch("https://qbkoftawrbmgqxsdzzqo.supabase.co/rest/v1/TSL", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia29mdGF3cmJtZ3F4c2R6enFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjcwMjUsImV4cCI6MjA0MTQ0MzAyNX0.WYYhWAAR - vH0tJEXbooEn5Wef00scdf - Qq8g8kiYFXs",
