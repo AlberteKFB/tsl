957cd3ba2bc436b73b3d04ab3044a625c8e488c3
Adam <adje0001@stud.kea.dk>
2024-09-10 13:37:38 +0200

    data virker

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
 
 //link med data
 //qbkoftawrbmgqxsdzzqo.supabase.co/rest/v1/TSL?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia29mdGF3cmJtZ3F4c2R6enFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjcwMjUsImV4cCI6MjA0MTQ0MzAyNX0.WYYhWAAR-vH0tJEXbooEn5Wef00scdf-Qq8g8kiYFXs
-https: fetch("https://qbkoftawrbmgqxsdzzqo.supabase.co/rest/v1/TSL", {
+
+fetch("https://qbkoftawrbmgqxsdzzqo.supabase.co/rest/v1/TSL", {
   method: "GET",
   headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia29mdGF3cmJtZ3F4c2R6enFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjcwMjUsImV4cCI6MjA0MTQ0MzAyNX0.WYYhWAAR - vH0tJEXbooEn5Wef00scdf - Qq8g8kiYFXs",
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFia29mdGF3cmJtZ3F4c2R6enFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjcwMjUsImV4cCI6MjA0MTQ0MzAyNX0.WYYhWAAR-vH0tJEXbooEn5Wef00scdf-Qq8g8kiYFXs",
   },
 })
   .then((res) => res.json())
